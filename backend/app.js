5b89d0a5c5ef042861f04c549c676ac8a5c5c4cb
Vladislav Kuznetsov <gloomy.fox123@gmail.com>
2023-01-14 23:32:40 +0300

    omg2

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.use(errors());
 // централизованный обработчик ошибок
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? `На сервере произошла ошибка ${err.message}` : message });
+  res.status(statusCode).send({ message: statusCode === 500 ? `На сервере произошла ошибка ${err}` : message });
   next();
 });
 
