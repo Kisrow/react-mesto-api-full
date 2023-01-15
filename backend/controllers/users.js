8c496df2a6346c1df0169231563a242171b2422c
Vladislav Kuznetsov <gloomy.fox123@gmail.com>
2023-01-15 16:32:16 +0300

    123123

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -109,7 +109,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'default-secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
       res.cookie('jwt', token, {
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'default-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new IncorrectAuthError('Необходима авторизация'));
   }
