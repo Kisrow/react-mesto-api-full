c4a3dd32cfed5a101b9b0f8cbbf773087bbcc34d
Vladislav Kuznetsov <gloomy.fox123@gmail.com>
2023-01-15 17:56:05 +0300

    no env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 const { IncorrectAuthError } = require('../erorrs/incorrect-auth');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'dev-secret');
   } catch (err) {
     return next(new IncorrectAuthError('Необходима авторизация'));
   }
