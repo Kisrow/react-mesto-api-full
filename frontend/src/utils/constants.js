e561697bd7dbd7f0f4c1686cf9301210a68f5b31
Vladislav Kuznetsov <gloomy.fox123@gmail.com>
2023-01-15 16:18:14 +0300

    env

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://kisrow.backend.nomoredomains.rocks' : 'http://localhost:3000'; 
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://kisrow.backend.nomoredomains.rocks'; 
 export default BASE_URL; 
